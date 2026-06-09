dca044aaf3c4b4a2bf75eb4411a26242c9a51cab
D0rag3on <noel.neuhoff@itc-studenten.de>
2026-06-09 13:26:29 +0200

    Shouldve been in the last commmit

---
+++ ssbackend/src/main/java/spot/safety/ssbackend/SsbackendApplication.java
@@ -0,0 +1,13 @@
+package spot.safety.ssbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsbackendApplication.class, args);
+    }
+
+}
---
+++ ssbackend/src/test/java/spot/safety/ssbackend/SsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package spot.safety.ssbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
