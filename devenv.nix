3d72b5604083dc2f95ccd4aa888de53e73894d27
oq_ <129159411+0qln@users.noreply.github.com>
2026-06-22 14:50:26 +0200

    Nix dev env (#50)
    * update gradle config
    
    * configure dev shell

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [javaPackages.compiler.temurin-bin.jdk-25];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/tasks/
+  tasks = {
+    "ss:test".exec = "(cd ssbackend && ./gradlew test)";
+    "ss:server".exec = "(cd ssbackend && ./gradlew bootRun)";
+    "ss:mobile".exec = "(cd ssmobile && ./gradlew run)"; # todo: start android emulator
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+  android.enable = true;
+}
