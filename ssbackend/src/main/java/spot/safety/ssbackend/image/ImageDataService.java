615519bb1e758ed7ae9af000df9a7ee07a6a8e19
oq_ <0qln@proton.me>
2026-06-25 17:20:53 +0200

    Add ImageData controller and service

---
+++ ssbackend/src/main/java/spot/safety/ssbackend/image/ImageDataService.java
@@ -0,0 +1,19 @@
+package spot.safety.ssbackend.image;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+import spot.safety.ssbackend.user.UserPrincipal;
+
+public interface ImageDataService {
+    /**
+     * Store binary data for an existing Image identified by id. Will replace any existing data.
+     * Also updates the associated Image.imageUrl to point to the data endpoint.
+     */
+    void store(Long id, MultipartFile file, UserPrincipal actor);
+
+    /**
+     * Load image data as a Spring Resource for serving.
+     */
+    Resource loadAsResource(Long id);
+}
+
